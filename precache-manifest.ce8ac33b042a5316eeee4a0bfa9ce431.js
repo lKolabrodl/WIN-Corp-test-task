3335853623b1fc0a3f5ad76dce24abff2ad6ce9c
Bril Mikhail <Kolabrod@mail.ru>
2020-08-24 17:25:31 +0300

    Updates

---
+++ precache-manifest.ce8ac33b042a5316eeee4a0bfa9ce431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7ef0653bae1dd639a0b03fb085be0f",
+    "url": "/WIN-Corp-test-task/index.html"
+  },
+  {
+    "revision": "5a656a422f19c182aa09",
+    "url": "/WIN-Corp-test-task/static/css/main.4490048b.chunk.css"
+  },
+  {
+    "revision": "b79bc146f1420e707ac5",
+    "url": "/WIN-Corp-test-task/static/js/2.88031406.chunk.js"
+  },
+  {
+    "revision": "ab6bd92c46b489bf0abe330868a5708e",
+    "url": "/WIN-Corp-test-task/static/js/2.88031406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a656a422f19c182aa09",
+    "url": "/WIN-Corp-test-task/static/js/main.c54b1eed.chunk.js"
+  },
+  {
+    "revision": "a862f9237cba48e7e69b",
+    "url": "/WIN-Corp-test-task/static/js/runtime-main.1ca79088.js"
+  }
+]);
